406b4a40fcba57d75aadc06887af29cac2f4ef42
edustocchero <edurevsky@gmail.com>
2023-06-01 16:17:49 -0300

    add natural

---
+++ natural.ml
@@ -0,0 +1,26 @@
+
+type natural =
+  | Zero
+  | Succ of natural
+
+let rec int_of_nat = function
+  | Zero -> 0
+  | Succ s -> 1 + int_of_nat s
+
+let rec plus x y =
+  match x, y with
+  | x, Zero -> x
+  | x, Succ s -> Succ (plus x s)
+
+(**
+
+plus (Succ Zero) (Succ (Succ Zero))    matches x, Succ s
+
+Succ (plus (Succ Zero) (Succ Zero))    matches x, Succ s
+
+Succ (Succ (plus (Succ Zero) (Zero)))  matches x, Zero
+
+Succ (Succ (Succ Zero))                return
+
+*)
+
